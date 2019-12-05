f8827b7b11584b76231c4645b4e35ef34dcbf8e8
kanandian <893979892@qq.com>
2019-12-05 14:47:20 +0800

    Initial Commit

---
+++ IOSTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSTest
+//
+//  Created by  macpro on 2019/12/5.
+//  Copyright © 2019 kanandian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
